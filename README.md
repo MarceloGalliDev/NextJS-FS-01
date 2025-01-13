1686691b7f6d2d52b4510cba9e5c232d3033ae40
marcelogallidev <marcelolemesgalli@hotmail.com>
2025-01-12 21:53:52 -0300

    feat: add table product

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # NextJS-FS-01
  
 - Criar Projeto Next
+
+### PRISMA
 - Instalar Prisma e dar npx prisma init
   - npx prisma format = para formatar o arquivo schema.prisma
   - npx prisma migrate dev --name init = criar uma migrate
   - npx prisma studio = abri um studio com o conteúdo do banco de dados
+  - npx prisma db push = ele aplica as migrate no banco de dados novo, por exemplo criar uma branch homolog
+  - npx prisma generate = para gerar o typescript do escopo da db
---
+++ stockly/app/products/_components/table-columns.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { Badge } from "@/app/_components/ui/badge"
+import { Product } from "@prisma/client"
+import { ColumnDef } from "@tanstack/react-table"
+import { CircleIcon } from "lucide-react";
+
+type ProductWithStatus = Product & {
+  status: string
+};
+
+const getStatusLabel = (status: string) => {
+  if (status === 'IN_STOCK') {
+    return 'Em estoque'
+  }
+  return 'Sem estoque'
+};
+
+export const productsTableColumns: ColumnDef<ProductWithStatus>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor Unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original
+      const label = getStatusLabel(product.status)
+      return (
+        <Badge
+          variant={label === 'Em estoque' ? 'default' : 'outline'}
+          className="gap-2"
+        >
+          <CircleIcon
+            size={14}
+            className={`${label === "Em estoque" ? "text-green-500 fill-green-500" : "text-red-500 fill-red-500"}`}
+          />
+          {label}
+        </Badge>)
+    }
+  },
+]
