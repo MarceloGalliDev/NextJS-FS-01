cdae222660dc201cb8e7e817ba8a630133b1115c
marcelogallidev <marcelolemesgalli@hotmail.com>
2025-01-14 23:19:49 -0300

    feat: create component ProductTableDropdownMenu

--- stockly/app/products/_components/table-columns.tsx
+++ stockly/app/products/_components/table-columns.tsx
@@ -1,27 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import { Badge } from "@/app/_components/ui/badge"
 import { Product } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { CircleIcon, ClipboardCopyIcon, EditIcon, MoreHorizontalIcon, TrashIcon } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/app/_components/ui/dropdown-menu"
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-} from "@/app/_components/ui/alert-dialog"
-
-import { Button } from "@/app/_components/ui/button";
-import DeleteProductsDialogContent from "./delete-dialog-content";
-import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
-import UpsertProductDialogContent from "./upsert-dialog-content";
+import { CircleIcon } from "lucide-react";
+import ProductTableDropdownMenu from "./table-dropdown-menu";
 
 
 const getStatusLabel = (status: string) => {
@@ -73,63 +56,6 @@ export const productsTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "actions",
     header: "Ações",
-    cell: (row) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const [editDialogOpen, setEditDialogOpen] = useState(false)
-      const product = row.row.original
-      return (
-        <AlertDialog>
-          <Dialog open={editDialogOpen} onOpenChange={setEditDialogOpen}>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost">
-                  <MoreHorizontalIcon size={16} />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Ações</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-
-                <DropdownMenuItem
-                  className="gap-1.5"
-                  onClick={() => navigator.clipboard.writeText(product.id)}
-                >
-                  <ClipboardCopyIcon size={16} />
-                  Copiar ID
-                </DropdownMenuItem>
-
-                <DialogTrigger asChild>
-                  <DropdownMenuItem
-                    className="gap-1.5"
-                  >
-                    <EditIcon size={16} />
-                    Editar
-                  </DropdownMenuItem>
-                </DialogTrigger>
-
-                <AlertDialogTrigger asChild>
-                  <DropdownMenuItem className="gap-1.5" >
-                    <TrashIcon size={16} />
-                    Excluir
-                  </DropdownMenuItem>
-                </AlertDialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <UpsertProductDialogContent
-              defaultValues={
-                {
-                  id: product.id,
-                  name: product.name,
-                  price: Number(product.price),
-                  stock: product.stock,
-                }
-              }
-              onSuccess={() => setEditDialogOpen(false)}
-            />
-            <DeleteProductsDialogContent productId={product.id} productName={product.name} />
-          </Dialog>
-        </AlertDialog>
-      )
-    }
+    cell: (row) => <ProductTableDropdownMenu product={row.row.original}/>
   }
 ]
